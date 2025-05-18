6b910d5227fb91172322ae2c6720d39917ba1c28
Meerim57 <marim.djoldoshova7@gmail.com>
2025-05-18 16:58:04 +0300

    team-members update

--- Frontend/src/app/pages/profile/team/team.component.ts
+++ Frontend/src/app/pages/profile/team/team.component.ts
@@ -1,7 +1,8 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterModule  } from '@angular/router';
-import { UsersService } from 'src/app/service/users.service';
+import { TeamMember, UsersService } from 'src/app/service/users.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-team',
@@ -10,15 +11,19 @@ import { UsersService } from 'src/app/service/users.service';
   standalone: true,
   imports: [
     MatIconModule,
-    RouterModule
+    RouterModule,
+    CommonModule
   ]
 })
 export class TeamComponent implements OnInit {
   usersService = inject(UsersService);
+  teamMembers: TeamMember[] = [];
 
   ngOnInit(): void {
     this.usersService.getUsers().subscribe({
-      next: (users) => console.log(users)
+      next: (users) => {
+        this.teamMembers = users.teamMembers;
+      }
     })
   }
 }
